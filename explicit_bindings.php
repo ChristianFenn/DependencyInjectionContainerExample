5ca9304491b77781df5ef7833ecb7f905405eb15
Christian Fenn <christianhfenn@gmail.com>
2023-03-11 11:05:52 -0800

    Use arrow functions

--- explicit_bindings.php
+++ explicit_bindings.php
@@ -12,26 +12,22 @@ use App\Services\SecurityClient;
 $container = new DependencyInjectionContainer();
 
 // OrderService needs a PaymentService and CustomerEngagementService. It can be instantiated 4th.
-$container->set(OrderService::class, function(DependencyInjectionContainer $c) {
-    return new OrderService($c->get(PaymentService::class), $c->get(CustomerEngagementService::class));
-});
+$container->set(OrderService::class, fn(DependencyInjectionContainer $c) =>
+    new OrderService($c->get(PaymentService::class), $c->get(CustomerEngagementService::class))
+);
 
 // PaymentService requires a SecurityClient. Instantiated 3nrd.
-$container->set(PaymentService::class, function(DependencyInjectionContainer $c) {
-    return new PaymentService($c->get(SecurityClient::class));
-});
+$container->set(PaymentService::class, fn(DependencyInjectionContainer $c) => 
+    new PaymentService($c->get(SecurityClient::class))
+);
 
 // Order of classes with no dependencies does not matter.
 
 // SecurityClient has no dependencies. It can be instantiated 2nd. 
-$container->set(SecurityClient::class, function(DependencyInjectionContainer $c) {
-    return new SecurityClient();
-});
+$container->set(SecurityClient::class, fn() => new SecurityClient());
 
 // CustomerEngagementService has no dependencies it can be instantiated 1st.
-$container->set(CustomerEngagementService::class, function(DependencyInjectionContainer $c) {
-    return new CustomerEngagementService();
-});
+$container->set(CustomerEngagementService::class, fn() => new CustomerEngagementService());
 
 $orderService = $container->get(OrderService::class);
 $orderService->process();
